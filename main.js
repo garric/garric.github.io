0d833a4b0704906b37112d064cd78066344a1afa
garric <ewd1986@163.com>
2017-05-09 19:52:04 +0800

    add tabs

--- main.js
+++ main.js
@@ -2,5 +2,30 @@
  * Created by garricxiang on 2017/5/8.
  */
 function run() {
-    learnWebGL.init()
+    var tabs = $('.navtab');
+    var containers = $('.contentContainer');
+    $('.contentContainer').hide();
+    $('.navtab').on('click', function() {
+        var tab = $(this)[0];
+        var index = -1;
+        for  (var i = 0; i < tabs.length; i++) {
+            containers[i].style.display = 'none';
+            containers[i].style.visibility = 'hidden';
+            tabs[i].style.color = '#000';
+
+            if (tab == tabs[i]) {
+                index = i;
+            }
+        }
+        if (index >= 0) {
+            containers[index].style.display = 'block';
+            containers[index].style.visibility = 'visible';
+            tabs[index].style.color = '#56A';
+        }
+
+    });
+
+    learnWebGL.init();
+    containers[0].style.display = 'block';
+    containers[0].style.visibility = 'visible';
 }
